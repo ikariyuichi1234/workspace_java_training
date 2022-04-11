38944f207999c61f2359d45b47fa1418dd1fe4f4
ikariyuichi1234 <ikari.yuuichi@ynet-inc.com>
2022-04-11 14:10:10 +0900

    4-2-3の作成

--- Chapter4-2/src/exam/Exam3.java
+++ Chapter4-2/src/exam/Exam3.java
@@ -1,9 +1,29 @@
 package exam;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Exam3 {
 
-	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+	public static void main (String[] args) {
+		ArrayList<String> textdata = new ArrayList<String>();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("文字列を入力してください");
+
+        while(true) {
+        	String text = sc.nextLine();
+            System.out.println("文字列を入力：" + text);
+            textdata.add(text);
+            
+            if(text.length() == 0) {
+            	break;
+            }
+        }
+        System.out.println("");
+        System.out.println(textdata);
+        
+        sc.close();
+
 
 	}
 
