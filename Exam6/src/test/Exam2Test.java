e45863796386faac92a803ec7d2e20a9a522b81e
ikariyuichi1234 <ikari.yuuichi@ynet-inc.com>
2022-04-13 16:19:06 +0900

    start_ch6

---
+++ Exam6/src/test/Exam2Test.java
@@ -0,0 +1,37 @@
+
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import exam.Exam2;
+
+class Exam2Test {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		assertEquals(Exam2.average(8,12), 10);
+	}
+
+}
